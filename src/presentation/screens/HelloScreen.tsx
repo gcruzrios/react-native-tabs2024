773f31d2ad62689b9130106a7dc0f1366d206b4c
gcruzrios <gcruzr@gmail.com>
2024-06-10 08:16:16 -0600

    directory body with hello

---
+++ src/presentation/screens/HelloScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+const HelloScreen = () => {
+  return (
+    <View>
+    <Text>HelloScreen</Text>
+    </View>
+    
+  )
+}
+
+export default HelloScreen
