263d6bf4606db2dd25900f7189f90eaa9f7f47bd
gcruzrios <gcruzr@gmail.com>
2024-06-10 08:34:25 -0600

    directory body with Styles

--- src/presentation/screens/HelloScreen.tsx
+++ src/presentation/screens/HelloScreen.tsx
@@ -1,12 +1,29 @@
-import {View, Text} from 'react-native'
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native'
 
 const HelloScreen = () => {
   return (
-    <View>
-    <Text>HelloScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
     
+    <View>
+    <Text style="styles.container">HelloScreen</Text>
+    </View>
+    </SafeAreaView>
   )
 }
 
+
 export default HelloScreen
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center'
+    },
+    title:{
+      fontSize:40,
+      textAlign:'center',
+      color:'black',
+      padding:20
+    }
+    
+})
