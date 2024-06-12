93c32121dd2eae262cd54aeaeaf25afb904a389b
gcruzrios <gcruzr@gmail.com>
2024-06-12 15:20:52 -0600

    Home and News Changes

--- src/presentation/screens/SettingsScreen.tsx
+++ src/presentation/screens/SettingsScreen.tsx
@@ -1,14 +1,44 @@
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native'
+import {SafeAreaView, StyleSheet, View,  Alert} from 'react-native'
+import {Avatar, Button, Card, Text} from 'react-native-paper';
 interface Props{
     name?: string;
 }
 
 const SettingsScreen = ({name = 'World'}: Props) => {
+
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  const createThreeButtonAlert = () =>
+    
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+
   return (
     
     <SafeAreaView style={styles.container}>
     <View>
     <Text style={styles.title}>Settings</Text>
+    <Button  onPress={createTwoButtonAlert}> 2 Botones de Alertas </Button>
+      <Button onPress={createThreeButtonAlert} >3 Botones de Alertas </Button>
     </View>
     </SafeAreaView>
   )
